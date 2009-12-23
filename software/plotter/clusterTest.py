cff2530a1473a96313600dff68d3b535482b41bd
Brett Graham <graham@rowland.harvard.edu>
2009-12-23 18:17:46 -0500

    clarified output of plotter program and added initialization points for kmeans clustering

--- software/plotter/clusterTest.py
+++ software/plotter/clusterTest.py
@@ -35,43 +35,112 @@ while l[:2] == 'v ':
 
 f.close()
 
-# cluster based on z measurements
-wz = whiten(z)
-#res, idx = kmeans2(wz,NClusters,iter=100,minit='points') # look for N steps
-res, idx = kmeans2(array(z),NClusters,iter=100,minit='points') # look for N steps
+def cluster_points(x,y,z,n):
+    wz = whiten(z)
+    kInit = linspace(min(wz), max(wz), n)
+    res, idx = kmeans2(wz, kInit, iter=100,minit='matrix')
+    
+    xs = [[] for i in xrange(n)]
+    ys = [[] for i in xrange(n)]
+    zs = [[] for i in xrange(n)]
+    
+    for i in xrange(len(idx)):
+        xs[idx[i]] += [x[i],]
+        ys[idx[i]] += [y[i],]
+        zs[idx[i]] += [z[i],]
+    
+    return xs, ys, zs
 
-# split clusters
-xs = [[] for i in range(NClusters)]
-ys = [[] for i in range(NClusters)]
-zs = [[] for i in range(NClusters)]
+xs, ys, zs = cluster_points(x,y,z,NClusters)
 
-for i in range(len(idx)):
-    xs[idx[i]] += [x[i],]
-    ys[idx[i]] += [y[i],]
-    zs[idx[i]] += [z[i],]
+# calculate 'fit'
+stDev = 0.0
+for i in xrange(NClusters):
+    stDev += std(xs[i])
+    stDev += std(ys[i])
+    stDev += std(zs[i])
+stDev /= 3
+print stDev
 
-#print "raw(non-aligned) z-difference:",mean(zs[1]) - mean(zs[0])
-print "raw(non-aligned) z-difference:",[abs(mean(zs[i]) - mean(zs[i-1])) for i in range(NClusters)]
+clusterFailures = 0
+while any([len(c) == 0 for c in xs]):
+    clusterFailures += 1
+    print "kmeans2 failed to find",NClusters,"clusters, trying again with",NClusters-clusterFailures
+    xs, ys, zs = cluster_points(x,y,z,NClusters-clusterFailures)
 
-print "aligning..."
-# calculate x and y tilt of stairs
-weighted_x_slopes = []
-weighted_y_slopes = []
+NClusters -= clusterFailures
+
+# # cluster based on z measurements
+# wz = whiten(z)
+# kInit = linspace(min(wz),max(wz),NClusters)
+# #res, idx = kmeans2(wz,NClusters,iter=100,minit='points') # look for N steps
+# res, idx = kmeans2(wz,kInit,iter=100,minit='matrix') # look for N steps
+# 
+# # split clusters
+# xs = [[] for i in range(NClusters)]
+# ys = [[] for i in range(NClusters)]
+# zs = [[] for i in range(NClusters)]
+# 
+# for i in range(len(idx)):
+#     xs[idx[i]] += [x[i],]
+#     ys[idx[i]] += [y[i],]
+#     zs[idx[i]] += [z[i],]
+# 
+# #print "raw(non-aligned) z-difference:",mean(zs[1]) - mean(zs[0])
+# #print "raw(non-aligned) z-difference:",[abs(mean(zs[i]) - mean(zs[i-1])) for i in range(NClusters)]
+
+def align_data(xs, ys, zs):
+    weighted_x_slopes = []
+    weighted_y_slopes = []
+    for i in range(len(xs)):
+        xr = linregress(xs[i], zs[i])
+        yr = linregress(ys[i], zs[i])
+        weighted_x_slopes += [xr[0] * len(xs[i]),]
+        weighted_y_slopes += [yr[0] * len(ys[i]),]
+    
+    NPoints = sum([len(x) for x in xs])
+    x_slope = sum(weighted_x_slopes)/NPoints
+    y_slope = sum(weighted_y_slopes)/NPoints
+    
+    for g in range(len(xs)):
+        for i in range(len(zs[g])):
+            zs[g][i] -= ys[g][i] * y_slope + xs[g][i] * x_slope
+    
+    return xs, ys, zs
+
+xs, ys, zs = align_data(xs, ys, zs)
+# #print "aligning..."
+# # calculate x and y tilt of stairs
+# weighted_x_slopes = []
+# weighted_y_slopes = []
+# for i in range(NClusters):
+#     xr = linregress(xs[i],zs[i])
+#     yr = linregress(ys[i],zs[i])
+#     weighted_x_slopes += [xr[0]*len(xs[i]),]
+#     weighted_y_slopes += [yr[0]*len(ys[i]),]
+# 
+# x_slope = sum(weighted_x_slopes)/len(x)
+# y_slope = sum(weighted_y_slopes)/len(y)
+# 
+# for g in range(NClusters):
+#     for i in range(len(zs[g])):
+#         zs[g][i] -= ys[g][i] * y_slope + xs[g][i] * x_slope
+
+#print "aligned z-difference:",[abs(mean(zs[i]) - mean(zs[i-1])) for i in range(NClusters)]
+clusterMeans = [mean(zs[i]) for i in range(NClusters)]
+clusterMeans.sort()
+#print "Z-Mean\tZ-Diffs"
+print "\t",
 for i in range(NClusters):
-    xr = linregress(xs[i],zs[i])
-    yr = linregress(ys[i],zs[i])
-    weighted_x_slopes += [xr[0]*len(xs[i]),]
-    weighted_y_slopes += [yr[0]*len(ys[i]),]
+    print "%.3f\t" % clusterMeans[i],
+print
+for i in range(NClusters):
+    print "%.3f\t" % clusterMeans[i],
+    for i2 in range(NClusters):
+        print "%.3f\t" % abs(clusterMeans[i] - clusterMeans[i2]),
+    print
 
-x_slope = sum(weighted_x_slopes)/len(x)
-y_slope = sum(weighted_y_slopes)/len(y)
-
-for g in range(NClusters):
-    for i in range(len(zs[g])):
-        zs[g][i] -= ys[g][i] * y_slope + xs[g][i] * x_slope
-
-print "aligned z-difference:",[abs(mean(zs[i]) - mean(zs[i-1])) for i in range(NClusters)]
-colors = ['b', 'r', 'g']
+colors = ['b', 'r', 'g', 'c', 'm']
 for i in range(NClusters):
     subplot(221)
     scatter(xs[i],ys[i],c=colors[i])
